049222ebbb56138b6493c02c18380130c03766ff
Maksperskiy <maksukay@mail.ru>
2022-04-05 20:29:44 +0300

    add count_users

--- muhanjan_loto/main/admin.py
+++ muhanjan_loto/main/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Game, Lobby, Player
 
+
+class LobbyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'max_users')
+
+
+
 admin.site.register(Game)
 admin.site.register(Lobby)
 admin.site.register(Player)
--- muhanjan_loto/main/views.py
+++ muhanjan_loto/main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.db.models import *
 
-from .models import Lobby
+
+from .models import Lobby, Game, Player
 
 
 def index(request):
-    lobbies = Lobby.objects.filter(is_active = True)
+    lobbies = Lobby.objects.filter(is_active = True) \
+        .annotate(count_users=Count('player'))
     return render(request, 'main/index.html', {'title': 'Список лобби', 'lobbies': lobbies})
