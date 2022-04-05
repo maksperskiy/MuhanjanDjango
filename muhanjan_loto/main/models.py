a809cacc9ddffe8fb8ed461b5c89e44eb4f8c734
Maksperskiy <maksukay@mail.ru>
2022-04-05 22:21:20 +0300

    add form validations
    rename field in player

--- muhanjan_loto/main/models.py
+++ muhanjan_loto/main/models.py
@@ -2,9 +2,13 @@ from datetime import datetime
 from django.conf import settings
 from django.db import models
 
+
 class Game(models.Model):
     name = models.CharField(max_length=50)
     max_users = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
 
 
 class Lobby(models.Model):
@@ -13,8 +17,13 @@ class Lobby(models.Model):
     date = models.DateTimeField(default=datetime.now())
     is_active = models.BooleanField(default=True)
     password = models.CharField(max_length=5, blank=True, null=True)
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        default=None, blank=True, null=True)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name_plural = 'Lobbies'
@@ -24,4 +33,7 @@ class Lobby(models.Model):
 class Player(models.Model):
     twitch_name = models.CharField(max_length=50)
     data = models.TextField()
-    game = models.ForeignKey(Lobby, on_delete=models.CASCADE)
+    lobby = models.ForeignKey(Lobby, on_delete=models.CASCADE)
+    
+    class Meta():
+        index_together = [['lobby']]
