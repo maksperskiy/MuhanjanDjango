16f2ab1a35c4a20da2e83c194ae5cf7e727bdaf5
Maksperskiy <maksukay@mail.ru>
2022-04-06 21:34:50 +0300

    add views

--- muhanjan_loto/loto/urls.py
+++ muhanjan_loto/loto/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('managelobby/<int:lobby_id>', views.manage_page, name='manage lobby')
-]
+    path('managelobby/<int:lobby_id>', views.manage_page, name='manage lobby'),
+    path('enterlobby/<int:lobby_id>', views.enter_lobby_page, name='enter lobby'),
+    path('card/<int:lobby_id>/', views.get_game_card, name='my card'),
+]   
--- muhanjan_loto/main/admin.py
+++ muhanjan_loto/main/admin.py
@@ -7,7 +7,6 @@ from django.contrib.admin.utils import quote
 
 from .forms import LobbyForm
 from .models import Game, Lobby, Player
-from loto.models import Card, Barrel, Winner, Stream
 
 
 class LobbyChangeList(ChangeList):
@@ -64,62 +63,3 @@ class GameAdmin(admin.ModelAdmin):
 admin.site.register(Game, GameAdmin)
 admin.site.register(Lobby, LobbyAdmin)
 admin.site.register(Player)
-
-
-@admin.action(description='Generate cards')
-def make_published(modeladmin, request, queryset):
-    print('asdasdasd')
-
-class CardAdmin(admin.ModelAdmin):
-    list_display = ['id', 'numbers']
-    actions = [make_published]
-
-
-
-
-
-admin.site.register(Card, CardAdmin)
-admin.site.register(Barrel)
-admin.site.register(Winner)
-admin.site.register(Stream)
-
-
-def get_app_list(self, request):
-    app_dict = self._build_app_dict(request)
-    app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
-
-    app_ordering = {
-        'main': 1, 
-        'auth': 2, 
-        'loto': 3
-        }
-    app_list.sort(key=lambda x: app_ordering[x['app_label']])
-    
-    for app in app_list:
-        if app['app_label'] == 'auth':
-            models_ordering = {
-                'Users': 1,
-                'Groups': 2
-            }
-            app['models'].sort(key=lambda x: models_ordering[x['name']])
-            
-        if app['app_label'] == 'main':
-            models_ordering = {
-                'Games': 1,
-                'Lobbies': 2,
-                'Players': 3,
-            }
-            app['models'].sort(key=lambda x: models_ordering[x['name']])
-            
-        if app['app_label'] == 'loto':
-            models_ordering = {
-                'Barrels': 1,
-                'Winners': 2,
-                'Streams': 3,
-                'Cards': 4
-            }
-            app['models'].sort(key=lambda x: models_ordering[x['name']])
-
-    return app_list
-
-admin.AdminSite.get_app_list = get_app_list
