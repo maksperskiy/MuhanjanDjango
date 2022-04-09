2fbac63e95dc1e97fa245b42d3b3fe86278460f8
MIRON <1111111111aleksey46@gmail.com>
2022-04-09 23:25:25 +0300

    fail

--- muhanjan_loto/loto/urls.py
+++ muhanjan_loto/loto/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
@@ -6,4 +6,11 @@ urlpatterns = [
     path('managelobby/<int:lobby_id>', views.manage_page, name='manage lobby'),
     path('enterlobby/<int:lobby_id>', views.enter_lobby_page, name='enter lobby'),
     path('card/<int:lobby_id>/', views.get_game_card, name='my card'),
+
+    path('managelobby/<int:lobby_id>/', views.stop_game, name='stop game'),
+    path('managelobby/<int:lobby_id>/get_stream', views.get_stream, name='get_stream'),
+    path('managelobby/<int:lobby_id>/', views.add_barrel, name='add_barrel'),
+
+
+    
 ]   
