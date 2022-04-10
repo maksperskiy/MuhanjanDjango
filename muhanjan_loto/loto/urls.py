1257b79980428bb9f55a935c71464e30d4e900a1
MIRON <1111111111aleksey46@gmail.com>
2022-04-10 17:33:52 +0300

    add ajax

--- muhanjan_loto/loto/urls.py
+++ muhanjan_loto/loto/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('enterlobby/<int:lobby_id>', views.enter_lobby_page, name='enter lobby'),
     path('card/<int:lobby_id>/', views.get_game_card, name='my card'),
 
-    path('managelobby/<int:lobby_id>/', views.stop_game, name='stop game'),
-    path('managelobby/<int:lobby_id>/get_stream', views.get_stream, name='get_stream'),
+    path('managelobby/<int:lobby_id>/', views.stop_game, name='stop_game'),
+    path('managelobby/<int:lobby_id>/', views.get_stream, name='get_stream'),
     path('managelobby/<int:lobby_id>/', views.add_barrel, name='add_barrel'),
 
 
