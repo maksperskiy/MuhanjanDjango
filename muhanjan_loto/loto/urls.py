2c2a294e5f11babef312cdab9ed24a1bf84b2f2c
Maksperskiy <maksukay@mail.ru>
2022-04-10 23:23:24 +0300

    refactor urls
    add remove_barrel
    add get_barrels
    
    implement add/remove/get barrels in template

--- muhanjan_loto/loto/urls.py
+++ muhanjan_loto/loto/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('enterlobby/<int:lobby_id>', views.enter_lobby_page, name='enter lobby'),
     path('card/<int:lobby_id>/', views.get_game_card, name='my card'),
 
-    path('managelobby/<int:lobby_id>/', views.stop_game, name='stop_game'),
-    path('managelobby/<int:lobby_id>/', views.get_stream, name='get_stream'),
-    path('managelobby/<int:lobby_id>/', views.add_barrel, name='add_barrel'),
+    path('managelobby/<int:lobby_id>/ajax/stop_game/', views.stop_game, name='stop_game'),
+    path('managelobby/<int:lobby_id>/ajax/get_stream/', views.get_stream, name='get_stream'),
+    path('managelobby/<int:lobby_id>/ajax/add_barrels/', views.get_barrels, name='get_barrels'),
+    path('managelobby/<int:lobby_id>/ajax/add_barrel/<int:number>', views.add_barrel, name='add_barrel'),
+    path('managelobby/<int:lobby_id>/ajax/remove_barrel/<int:number>/', views.remove_barrel, name='remove_barrel'),
 
 
     
